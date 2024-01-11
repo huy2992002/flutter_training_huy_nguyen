db9551de7c10411ad1fc855bf38b38ec4fd28a90
huy2992002 <huy2992018@gmail.com>
2024-01-11 14:45:40 +0700

    add examples dart:developer

---
+++ dart_examples/lib/dart_core_libraries/examples_dart_developer.dart
@@ -0,0 +1,4 @@
+void main(List<String> args) {
+    // This is a specialized library intended for interacting with the Dart runtime programmatically for debugging and inspection. 
+    // Sample uses include advanced debugging, and creating developer tools.
+}
