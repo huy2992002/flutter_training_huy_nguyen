45ceaa925021c56dcc808f881cc3854bcf7707f8
huy2992002 <huy2992018@gmail.com>
2024-01-12 09:00:12 +0700

    update examples enums

--- dart_examples/lib/class_object.dart/examples_enums.dart
+++ dart_examples/lib/class_object.dart/examples_enums.dart
@@ -1,6 +1,9 @@
 void main(List<String> args) {
   print(Color.red.name); // red
-  print(Color.green.name); // red
+  print(Color.green.name); // green
+
+  Color a = Color.blue;
+  print(a);
 }
 
 // create enums Color
