3612c3a74e7385d4215e67426f0f2b8e43c558ea
huy2992002 <huy2992018@gmail.com>
2024-01-17 11:30:16 +0700

    add exercise 13

---
+++ dart_practice/lib/exercise_13/main.dart
@@ -0,0 +1,34 @@
+import 'package:dart_practice/exercise_13/manager/product_manager.dart';
+import 'package:dart_practice/exercise_13/models/product_model.dart';
+import 'package:dart_practice/utils/validator.dart';
+
+void main(List<String> args) {
+  final productManager = ProductManager(initProducts);
+
+  int i;
+
+  do {
+    productManager.printMenu();
+    do {
+      i = Validator.inputInt('Choice: ');
+      if (i < 1 || i > 7) print('Please enter from 1 to 7 !!!');
+    } while (i < 1 || i > 7);
+
+    switch (i) {
+      case 1:
+        productManager.showAllListProducts();
+      case 2:
+        productManager.createProduct();
+      case 3:
+        productManager.editProduct();
+      case 4:
+        productManager.deleteProduct();
+      case 5:
+        productManager.searchProduct();
+      case 6:
+        productManager.countTotalProducts();
+      case 7:
+        print('Program is finished');
+    }
+  } while (i != 7);
+}
---
+++ dart_practice/lib/exercise_13/manager/product_manager.dart
@@ -0,0 +1,76 @@
+import 'package:dart_practice/exercise_13/models/product_model.dart';
+import 'package:dart_practice/utils/validator.dart';
+
+class ProductManager {
+  ProductManager(this._products);
+  final List<ProductModel> _products;
+
+  void printMenu() {
+    print('Choose one of the options below.');
+    print('1. View all list products.');
+    print('2. Create product.');
+    print('3. Edit product by ID.');
+    print('4. Delete product by ID.');
+    print('5. Search product by name.');
+    print('6. Count the total products in the list.');
+    print('7. Exit.');
+  }
+
+  void showAllListProducts() {
+    print('Show All List products: ');
+    for (final product in _products) {
+      print(product);
+    }
+  }
+
+  void createProduct() {
+    print('Create new Product.');
+    final product = ProductModel()..inputInfomation();
+    _products.add(product);
+    print('Create Product successfully !!!');
+  }
+
+  void editProduct() {
+    final id = Validator.inputInt('Enter the id you want to edit: ');
+
+    for (final product in _products) {
+      if (product.id == id) {
+        product.inputInfomation();
+        print('Edit product successfully!!!');
+        return;
+      }
+    }
+
+    print('Could not find product id to edit.');
+  }
+
+  void deleteProduct() {
+    final id = Validator.inputInt('Enter the id you want to delete: ');
+
+    for (final product in _products) {
+      if (product.id == id) {
+        _products.remove(product);
+        print('Delete product successfully!!!');
+        return;
+      }
+    }
+
+    print('Could not find product id to delete.');
+  }
+
+  void searchProduct() {
+    final name = Validator.inputString(
+        'Enter the product name you want to search for: ');
+
+    final listSearch = _products
+        .where((e) => e.name.toLowerCase().contains(name.toLowerCase()))
+        .toList();
+        
+    print('List of products found: ');
+    listSearch.forEach(print);
+  }
+
+  void countTotalProducts() {
+    print('Total products in the list is ${_products.length}');
+  }
+}
