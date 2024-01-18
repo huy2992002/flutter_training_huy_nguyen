70681c26a347f6447e5c3f041b90b50a9bddd8bd
huy2992002 <huy2992018@gmail.com>
2024-01-18 10:02:14 +0700

    update exercise 12

--- dart_practice/lib/exercise_12.1.dart
+++ dart_practice/lib/exercise_12.1.dart
@@ -1,6 +1,7 @@
 //Load the birthday dictionary from a JSON file on disk, rather than having the dictionary defined in the program.
 
-import 'package:dart_practice/utils/services.dart';
+import 'dart:convert';
+import 'dart:io';
 import 'package:dart_practice/utils/validator.dart';
 
 void main(List<String> args) {
@@ -9,8 +10,20 @@ void main(List<String> args) {
   getBirthDay(path);
 }
 
+Map<String, dynamic> getDictionary(String path) {
+  final data = File(path).readAsStringSync();
+  final dictionary = jsonDecode(data) as Map<String, dynamic>;
+  return dictionary;
+}
+
+void saveDictionary(Map<String, dynamic> dictionary, String path) {
+  final file = File(path);
+  final content = json.encode(dictionary);
+  file.writeAsStringSync(content);
+}
+
 void getBirthDay(String path) {
-  final dictionary = getDataJson(path);
+  final dictionary = getDictionary(path);
   final name = Validator.inputString("who's birthday do you want look up?: ");
   if (dictionary[name] != null) {
     print("$name's birthday is ${dictionary[name]}");
@@ -19,6 +32,6 @@ void getBirthDay(String path) {
         Validator.inputBirthday("Please update $name's birthday: ");
     dictionary.putIfAbsent(name, () => birthday);
     print('Update successfully');
-    saveDataJson(dictionary, path);
+    saveDictionary(dictionary, path);
   }
 }
