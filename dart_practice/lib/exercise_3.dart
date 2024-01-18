26e22f75e13b9b8384117f6ec277cf93ac262a68
huy2992002 <huy2992018@gmail.com>
2024-01-18 15:05:48 +0700

    fix exercise 3

--- dart_practice/lib/exercise_3.dart
+++ dart_practice/lib/exercise_3.dart
@@ -5,14 +5,23 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  final a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-  itemLess5InList(a);
-}
-
-void itemLess5InList(List<int> list) {
-  for (final e in list) {
-    if(e < 5) {
+  final list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  final results = itemLess5InList(list);
+  if (results.isNotEmpty) {
+    for (final e in results) {
       stdout.write('$e ');
     }
+  } else {
+    print('No element is less than 5');
   }
+}
+
+List<int> itemLess5InList(List<int> list) {
+  final results = <int>[];
+  for (final e in list) {
+    if (e < 5) {
+      results.add(e);
+    }
+  }
+  return results;
 }
