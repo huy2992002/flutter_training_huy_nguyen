f40c5484974c4187edcc756fe34c66856dc6bebb
huy2992002 <huy2992018@gmail.com>
2024-01-22 08:58:02 +0700

    update path constants

--- dart_practice/lib/utils/constants.dart
+++ dart_practice/lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class Constants  {
-  static const productDataPath = 'dart_practice/lib/exercise_13/data/product_data.json';
-  static const cartDataPath = 'dart_practice/lib/exercise_13/data/cart_data.json';
+  static const productDataPath = 'dart_practice/lib/ex13_product_management_app/data/product_data.json';
+  static const cartDataPath = 'dart_practice/lib/ex13_product_management_app/data/cart_data.json';
   static const dataPath = 'dart_practice/lib/data.json';
 } 
