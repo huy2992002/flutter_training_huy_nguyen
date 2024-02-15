a24af68abc8fbeaf06cf77cb782791cbfb249536
huy2992002 <huy2992018@gmail.com>
2024-02-15 15:10:59 +0700

    implement create component NSTextButton

---
+++ nike_sneaker_store/lib/components/ns_text_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:nike_sneaker_store/resources/ns_style.dart';
+
+class NsTextButton extends StatelessWidget {
+  const NsTextButton({
+    required this.onPressed,
+    required this.text,
+    this.textStyle,
+    super.key,
+  });
+
+  final Function() onPressed;
+  final String text;
+  final TextStyle? textStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Text(
+        text,
+        style: textStyle ?? NSStyle.h16Medium,
+      ),
+    );
+  }
+}
