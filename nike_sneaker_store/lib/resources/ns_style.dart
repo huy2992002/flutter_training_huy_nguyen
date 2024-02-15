ad7a9d471f00e64e2ed1b30bce4624cb3ee67635
huy2992002 <huy2992018@gmail.com>
2024-02-15 11:30:03 +0700

    add fonts family in app

---
+++ nike_sneaker_store/lib/resources/ns_style.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class NSStyle {
+  NSStyle._();
+
+  static const h12Normal = TextStyle(
+    fontSize: 12,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const h12Medium = TextStyle(
+    fontSize: 12,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const h14Normal = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const h14Medium = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const h14SemiBold = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const h16ExtraLight = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.w200,
+  );
+
+  static const h16Normal = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const h16Medium = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.w500,
+  );
+  static const h16SemiBold = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const h16Bold = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.w700,
+  );
+
+  static const h18Semibold = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const h20Medium = TextStyle(
+    fontSize: 20,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const h21SemiBold = TextStyle(
+    fontSize: 21,
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const h24Semibold = TextStyle(
+    fontSize: 24,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const h26Bold = TextStyle(
+    fontSize: 26,
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.w700,
+  );
+
+  static const h32Bold = TextStyle(
+    fontSize: 32,
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.w700,
+  );
+
+  static const h32Black = TextStyle(
+    fontSize: 32,
+    fontFamily: 'Raleway',
+    fontWeight: FontWeight.w900,
+  );
+}
