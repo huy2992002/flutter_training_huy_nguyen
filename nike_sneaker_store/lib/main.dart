2a95d8635e3caf5621e386eebb5f641c5a1d670f
huy2992002 <huy2992018@gmail.com>
2024-02-15 10:26:40 +0700

    apply theme data in app

--- nike_sneaker_store/lib/main.dart
+++ nike_sneaker_store/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nike_sneaker_store/l10n/app_localizations.dart';
+import 'package:nike_sneaker_store/themes/ns_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,10 +16,8 @@ class MyApp extends StatelessWidget {
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       locale: AppLocalizations.supportedLocales[1],
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: NSTheme.lightTheme,
+      darkTheme: NSTheme.darkTheme,
       home: Container(),
     );
   }
---
+++ nike_sneaker_store/lib/themes/ns_theme.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:nike_sneaker_store/resources/ns_color.dart';
+
+class NSTheme {
+  NSTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: const ColorScheme(
+      brightness: Brightness.light,
+      primary: NSColor.primary,
+      onPrimary: NSColor.onPrimary,
+      secondary: NSColor.secondary,
+      onSecondary: NSColor.onSecondary,
+      error: NSColor.notification,
+      onError: NSColor.onPrimary,
+      background: NSColor.background,
+      onBackground: NSColor.onBackground,
+      surface: NSColor.surface,
+      onSurface: NSColor.onSurface,
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    colorScheme: const ColorScheme(
+      brightness: Brightness.dark,
+      primary: NSColor.darkPrimary,
+      onPrimary: NSColor.darkOnPrimary,
+      secondary: NSColor.darkSecondary,
+      onSecondary: NSColor.darkOnSecondary,
+      error: NSColor.notification,
+      onError: NSColor.onPrimary,
+      background: NSColor.darkBackground,
+      onBackground: NSColor.darkOnBackground,
+      surface: NSColor.surface,
+      onSurface: NSColor.onSurface,
+    ),
+  );
+}
