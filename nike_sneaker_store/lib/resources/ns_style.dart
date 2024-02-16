1d538072371d4b4879cd79563c861f1b99cba09d
huy2992002 <huy2992018@gmail.com>
2024-02-16 09:26:42 +0700

    update common style in app

--- nike_sneaker_store/lib/resources/ns_style.dart
+++ nike_sneaker_store/lib/resources/ns_style.dart
@@ -1,104 +1,115 @@
 import 'package:flutter/material.dart';
 
 class NSStyle {
-  NSStyle._();
+  static TextStyle primaryTextStyle({
+    required double fontSize,
+    required FontWeight fontWeight,
+    required String fontFamily,
+  }) {
+    return TextStyle(
+      fontSize: fontSize,
+      fontWeight: fontWeight,
+      fontFamily: fontFamily,
+    );
+  }
 
-  static const h12Normal = TextStyle(
+  static TextStyle h12Normal = primaryTextStyle(
     fontSize: 12,
     fontFamily: 'Poppins',
     fontWeight: FontWeight.w400,
   );
 
-  static const h12Medium = TextStyle(
+  static TextStyle h12Medium = primaryTextStyle(
     fontSize: 12,
     fontFamily: 'Poppins',
     fontWeight: FontWeight.w500,
   );
 
-  static const h14Normal = TextStyle(
+  static TextStyle h14Normal = primaryTextStyle(
     fontSize: 14,
     fontFamily: 'Poppins',
     fontWeight: FontWeight.w400,
   );
 
-  static const h14Medium = TextStyle(
+  static TextStyle h14Medium = primaryTextStyle(
     fontSize: 14,
     fontFamily: 'Poppins',
     fontWeight: FontWeight.w500,
   );
 
-  static const h14SemiBold = TextStyle(
+  static TextStyle h14SemiBold = primaryTextStyle(
     fontSize: 14,
     fontFamily: 'Raleway',
     fontWeight: FontWeight.w600,
   );
 
-  static const h16ExtraLight = TextStyle(
+  static TextStyle h16ExtraLight = primaryTextStyle(
     fontSize: 16,
     fontFamily: 'Raleway',
     fontWeight: FontWeight.w200,
   );
 
-  static const h16Normal = TextStyle(
+  static TextStyle h16Normal = primaryTextStyle(
     fontSize: 16,
     fontFamily: 'Poppins',
     fontWeight: FontWeight.w400,
   );
 
-  static const h16Medium = TextStyle(
+  static TextStyle h16Medium = primaryTextStyle(
     fontSize: 16,
     fontFamily: 'Raleway',
     fontWeight: FontWeight.w500,
   );
-  static const h16SemiBold = TextStyle(
+
+  static TextStyle h16SemiBold = primaryTextStyle(
     fontSize: 16,
     fontFamily: 'Raleway',
     fontWeight: FontWeight.w600,
   );
 
-  static const h16Bold = TextStyle(
+  static TextStyle h16Bold = primaryTextStyle(
     fontSize: 16,
     fontFamily: 'Raleway',
     fontWeight: FontWeight.w700,
   );
 
-  static const h18Semibold = TextStyle(
+  static TextStyle h18Semibold = primaryTextStyle(
     fontSize: 18,
     fontFamily: 'Poppins',
     fontWeight: FontWeight.w600,
   );
 
-  static const h20Medium = TextStyle(
+  static TextStyle h20Medium = primaryTextStyle(
     fontSize: 20,
     fontFamily: 'Poppins',
     fontWeight: FontWeight.w500,
   );
 
-  static const h21SemiBold = TextStyle(
+  static TextStyle h21SemiBold = primaryTextStyle(
     fontSize: 21,
     fontFamily: 'Raleway',
     fontWeight: FontWeight.w600,
   );
 
-  static const h24Semibold = TextStyle(
+  static TextStyle h24Semibold = primaryTextStyle(
     fontSize: 24,
     fontFamily: 'Poppins',
     fontWeight: FontWeight.w600,
   );
 
-  static const h26Bold = TextStyle(
+  static TextStyle h26Bold = primaryTextStyle(
     fontSize: 26,
     fontFamily: 'Raleway',
     fontWeight: FontWeight.w700,
   );
 
-  static const h32Bold = TextStyle(
+  static TextStyle h32Bold = primaryTextStyle(
     fontSize: 32,
     fontFamily: 'Raleway',
     fontWeight: FontWeight.w700,
   );
 
-  static const h32Black = TextStyle(
+  static TextStyle h32Black = primaryTextStyle(
     fontSize: 32,
     fontFamily: 'Raleway',
     fontWeight: FontWeight.w900,
