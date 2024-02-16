5ea5e95aff6685c2ead37f52a3a971dc00f51a02
huy2992002 <huy2992018@gmail.com>
2024-02-16 11:34:28 +0700

    create SplashPage temp

--- nike_sneaker_store/lib/pages/splash_page.dart
+++ nike_sneaker_store/lib/pages/splash_page.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:nike_sneaker_store/components/button/ns_elevated_button.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NSElevatedButton.text(onPressed: () {
-        
-      }, text: 'text'),
+    return const Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [],
+      ),
     );
   }
 }
