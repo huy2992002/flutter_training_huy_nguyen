3d7421938b73a138d7a32be4ddd2bac257643f2c
huy2992002 <huy2992018@gmail.com>
2024-02-16 09:48:13 +0700

    remove config for macOS, linux and window

--- nike_sneaker_store/lib/pages/splash_page.dart
+++ nike_sneaker_store/lib/pages/splash_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:nike_sneaker_store/components/button/ns_elevated_button.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return Scaffold(
+      body: NSElevatedButton.text(onPressed: () {
+        
+      }, text: 'text'),
+    );
   }
 }
