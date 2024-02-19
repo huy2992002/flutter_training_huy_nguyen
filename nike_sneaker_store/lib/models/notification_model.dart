51920c311635e77acbbde9b0e6529f540f1eac2a
huy2992002 <huy2992018@gmail.com>
2024-02-19 14:24:19 +0700

    update id object notification model

--- nike_sneaker_store/lib/models/notification_model.dart
+++ nike_sneaker_store/lib/models/notification_model.dart
@@ -2,13 +2,15 @@ import 'package:nike_sneaker_store/models/product_model.dart';
 
 class NotificationModel {
   NotificationModel({
+    required this.id,
     required this.title,
     required this.product,
     required this.priceSale,
     required this.date,
     this.isRead = false,
   });
-  
+
+  String id;
   String title;
   ProductModel product;
   double priceSale;
