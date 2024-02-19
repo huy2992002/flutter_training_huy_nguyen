2606b38e66bdf68db7015438184a2929ac6fb407
huy2992002 <huy2992018@gmail.com>
2024-02-19 15:05:56 +0700

    wait 2 second from splash to onboarding

--- nike_sneaker_store/lib/pages/splash_page.dart
+++ nike_sneaker_store/lib/pages/splash_page.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:nike_sneaker_store/gen/assets.gen.dart';
+import 'package:nike_sneaker_store/pages/onboarding_page.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    toPage();
+  }
+
+  Future<void> toPage() async {
+    await Future.delayed(const Duration(milliseconds: 2300), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnboardingPage(),
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +33,7 @@ class SplashPage extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.scrim,
       body: Center(
         child: Image.asset(Assets.images.imgLogo.path),
-      )
+      ),
     );
   }
 }
