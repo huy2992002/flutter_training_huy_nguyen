dd2185c5db9440bc47d984f2504df42bf4ef6bbd
huy2992002 <huy2992018@gmail.com>
2024-02-19 14:55:40 +0700

    implement code UI SplashPage

--- nike_sneaker_store/lib/pages/splash_page.dart
+++ nike_sneaker_store/lib/pages/splash_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:nike_sneaker_store/gen/assets.gen.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.scrim,
+      body: Center(
+        child: Image.asset(Assets.images.imgLogo.path),
+      )
+    );
   }
 }
