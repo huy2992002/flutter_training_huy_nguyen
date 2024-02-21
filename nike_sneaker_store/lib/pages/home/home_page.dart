3e95e982e3fe0a89ba33fc52a73cec9f8557bb13
huy2992002 <huy2992018@gmail.com>
2024-02-21 17:13:59 +0700

    update TitleHome

--- nike_sneaker_store/lib/pages/home/home_page.dart
+++ nike_sneaker_store/lib/pages/home/home_page.dart
@@ -90,7 +90,10 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           const SizedBox(height: 24),
-          TitleHome(text: AppLocalizations.of(context).newArrivals),
+          TitleHome(
+            text: AppLocalizations.of(context).newArrivals,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: CardSale(
--- nike_sneaker_store/lib/pages/home/widgets/title_home.dart
+++ nike_sneaker_store/lib/pages/home/widgets/title_home.dart
@@ -4,15 +4,17 @@ import 'package:nike_sneaker_store/resources/ns_style.dart';
 class TitleHome extends StatelessWidget {
   const TitleHome({
     required this.text,
+    this.padding,
     super.key,
   });
 
   final String text;
+  final EdgeInsetsGeometry? padding;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 20, bottom: 14),
+      padding: padding ?? const EdgeInsets.only(left: 20, bottom: 14),
       child: Text(
         text,
         style: NSStyle.h16SemiBold,
