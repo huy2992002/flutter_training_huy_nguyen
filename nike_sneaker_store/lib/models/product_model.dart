f08d87f23c9cb08fa3a0a253beea9e7290cc7015
huy2992002 <huy2992018@gmail.com>
2024-02-21 15:06:34 +0700

    fetch data products

--- nike_sneaker_store/lib/models/product_model.dart
+++ nike_sneaker_store/lib/models/product_model.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:nike_sneaker_store/gen/assets.gen.dart';
+import 'package:nike_sneaker_store/l10n/app_localizations.dart';
+import 'package:nike_sneaker_store/utils/maths.dart';
+
 class ProductModel {
   ProductModel({
     required this.uuid,
@@ -7,6 +12,7 @@ class ProductModel {
     required this.quantity,
     required this.description,
     required this.isBestSeller,
+    required this.category,
     this.isFavorite = false,
   });
 
@@ -17,5 +23,61 @@ class ProductModel {
   int quantity;
   String description;
   bool isBestSeller;
+  String category;
   bool isFavorite;
+}
+
+List<ProductModel> products(BuildContext context) {
+  return [
+    ProductModel(
+      uuid: Maths.randomUUid(length: 6),
+      name: AppLocalizations.of(context).nameNikeJordan,
+      imagePath: Assets.images.imgNikeJordan.path,
+      price: 302,
+      quantity: 20,
+      description: AppLocalizations.of(context).descriptionNikeJordan,
+      isBestSeller: true,
+      category: AppLocalizations.of(context).outDoor,
+    ),
+    ProductModel(
+      uuid: Maths.randomUUid(length: 6),
+      name: AppLocalizations.of(context).nameNikeAirMax,
+      imagePath: Assets.images.imgNikeAirMax.path,
+      price: 302,
+      quantity: 20,
+      description: AppLocalizations.of(context).descriptionNikeAirMax,
+      isBestSeller: true,
+      category: AppLocalizations.of(context).outDoor,
+    ),
+    ProductModel(
+      uuid: Maths.randomUUid(length: 6),
+      name: AppLocalizations.of(context).nameNikeAirMax200,
+      imagePath: Assets.images.imgNikeAirMax200.path,
+      price: 302,
+      quantity: 20,
+      description: AppLocalizations.of(context).descriptionNikeAirMax200,
+      isBestSeller: true,
+      category: AppLocalizations.of(context).outDoor,
+    ),
+    ProductModel(
+      uuid: Maths.randomUUid(length: 6),
+      name: AppLocalizations.of(context).nameNikeAirMax270,
+      imagePath: Assets.images.imgNikeAirMax270.path,
+      price: 302,
+      quantity: 20,
+      description: AppLocalizations.of(context).descriptionNikeAirMax270,
+      isBestSeller: true,
+      category: AppLocalizations.of(context).tennis,
+    ),
+    ProductModel(
+      uuid: Maths.randomUUid(length: 6),
+      name: AppLocalizations.of(context).nameNikeClubMax,
+      imagePath: Assets.images.imgNikeAirMax270.path,
+      price: 302,
+      quantity: 20,
+      description: AppLocalizations.of(context).descriptionNikeClubMax,
+      isBestSeller: true,
+      category: AppLocalizations.of(context).tennis,
+    ),
+  ];
 }
