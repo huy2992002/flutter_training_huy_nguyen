e6e8cf5c27843569747a89fbd1e823309fb303e1
huy2992002 <huy2992018@gmail.com>
2024-02-22 14:47:19 +0700

    update Favoirite ontap product push to view detail product

--- nike_sneaker_store/lib/pages/favorite/favorite_page.dart
+++ nike_sneaker_store/lib/pages/favorite/favorite_page.dart
@@ -6,6 +6,7 @@ import 'package:nike_sneaker_store/components/cards/card_product.dart';
 import 'package:nike_sneaker_store/gen/assets.gen.dart';
 import 'package:nike_sneaker_store/l10n/app_localizations.dart';
 import 'package:nike_sneaker_store/models/product_model.dart';
+import 'package:nike_sneaker_store/pages/detail/detail_page.dart';
 
 class FavoritePage extends StatefulWidget {
   const FavoritePage({super.key});
@@ -59,6 +60,12 @@ class _FavoritePageState extends State<FavoritePage> {
             product: product,
             onFavorite: () =>
                 setState(() => product.isFavorite = !product.isFavorite),
+            onTap: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => DetailPage(product: product),
+              ),
+            ),
           );
         },
       ),
