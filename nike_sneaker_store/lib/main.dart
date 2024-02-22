d2cc914a3ad81acd80fedb5d0d099c7b859dd044
huy2992002 <huy2992018@gmail.com>
2024-02-22 17:07:54 +0700

    update locale in project

--- nike_sneaker_store/lib/main.dart
+++ nike_sneaker_store/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      locale: AppLocalizations.supportedLocales[0],
+      locale: const Locale('en'),
       theme: NSTheme.lightTheme,
       darkTheme: NSTheme.darkTheme,
       home: const SplashPage(),
