781526c537faf0082e642d2d41b022f152245991
huy2992002 <huy2992018@gmail.com>
2024-02-22 14:45:20 +0700

    implement create UI FavoritePage

--- nike_sneaker_store/lib/pages/favorite/favorite_page.dart
+++ nike_sneaker_store/lib/pages/favorite/favorite_page.dart
@@ -1,14 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:nike_sneaker_store/resources/ns_color.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:nike_sneaker_store/components/app_bar/ns_app_bar.dart';
+import 'package:nike_sneaker_store/components/button/ns_icon_button.dart';
+import 'package:nike_sneaker_store/components/cards/card_product.dart';
+import 'package:nike_sneaker_store/gen/assets.gen.dart';
+import 'package:nike_sneaker_store/l10n/app_localizations.dart';
+import 'package:nike_sneaker_store/models/product_model.dart';
 
-class FavoritePage extends StatelessWidget {
+class FavoritePage extends StatefulWidget {
   const FavoritePage({super.key});
 
   @override
+  State<FavoritePage> createState() => _FavoritePageState();
+}
+
+class _FavoritePageState extends State<FavoritePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: NSColor.favorite,
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: NSAppBar(
+        title: AppLocalizations.of(context).favorite,
+        rightIcon: Stack(
+          children: [
+            NsIconButton(
+              onPressed: () {},
+              backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+              icon: SvgPicture.asset(
+                Assets.icons.icBag,
+                color: Theme.of(context).colorScheme.onPrimaryContainer,
+              ),
+            ),
+            Positioned(
+              right: 0,
+              child: Container(
+                width: 10,
+                height: 10,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.error,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: GridView.builder(
+        itemCount: products.length,
+        padding: const EdgeInsets.symmetric(horizontal: 20).copyWith(top: 28),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            childAspectRatio: 5 / 6),
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return CardProduct(
+            product: product,
+            onFavorite: () =>
+                setState(() => product.isFavorite = !product.isFavorite),
+          );
+        },
       ),
     );
   }
