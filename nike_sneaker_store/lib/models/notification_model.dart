1d7c327610d3df0317f9498d04c250da00f256f5
huy2992002 <huy2992018@gmail.com>
2024-02-23 15:53:09 +0700

    fetch data notifications

--- nike_sneaker_store/lib/models/notification_model.dart
+++ nike_sneaker_store/lib/models/notification_model.dart
@@ -1,4 +1,5 @@
 import 'package:nike_sneaker_store/models/product_model.dart';
+import 'package:nike_sneaker_store/utils/maths.dart';
 
 class NotificationModel {
   NotificationModel({
@@ -17,3 +18,27 @@ class NotificationModel {
   bool isRead;
   String date;
 }
+
+List<NotificationModel> notifications = [
+  NotificationModel(
+    uuid: Maths.randomUUid(length: 6),
+    title: 'We Have New Products With Offers',
+    product: products[0],
+    priceSale: 250,
+    date: '2024-02-23',
+  ),
+  NotificationModel(
+    uuid: Maths.randomUUid(length: 6),
+    title: 'We Have New Products With Offers',
+    product: products[4],
+    priceSale: 280,
+    date: '2024-02-22',
+  ),
+  NotificationModel(
+    uuid: Maths.randomUUid(length: 6),
+    title: 'We Have New Products With Offers',
+    product: products[2],
+    priceSale: 290,
+    date: '2024-02-19',
+  ),
+];
