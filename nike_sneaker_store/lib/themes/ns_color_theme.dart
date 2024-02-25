eeb60715d90b41e6d2079cb4752d779037f81f11
huy2992002 <huy2992018@gmail.com>
2024-02-25 14:23:04 +0700

    create textTheme in project

---
+++ nike_sneaker_store/lib/themes/ns_color_theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:nike_sneaker_store/resources/ns_color.dart';
+
+class NSColorTheme {
+  static ColorScheme colorThemeLight = const ColorScheme(
+    brightness: Brightness.light,
+    primary: NSColor.primary,
+    onPrimary: NSColor.onPrimary,
+    secondary: NSColor.secondary,
+    onSecondary: NSColor.onSecondary,
+    error: NSColor.notification,
+    onError: NSColor.onPrimary,
+    background: NSColor.background,
+    onBackground: NSColor.onBackground,
+    surface: NSColor.surface,
+    onSurface: NSColor.onSurface,
+    primaryContainer: NSColor.primaryContainer,
+    onPrimaryContainer: NSColor.onPrimaryContainer,
+    secondaryContainer: NSColor.secondaryContainer,
+    onSecondaryContainer: NSColor.onSecondaryContainer,
+    scrim: NSColor.primary,
+    tertiary: NSColor.tertiary,
+  );
+
+  static ColorScheme colorThemeDark = const ColorScheme(
+    brightness: Brightness.dark,
+    primary: NSColor.darkPrimary,
+    onPrimary: NSColor.darkOnPrimary,
+    secondary: NSColor.darkSecondary,
+    onSecondary: NSColor.darkOnSecondary,
+    error: NSColor.notification,
+    onError: NSColor.onPrimary,
+    background: NSColor.darkBackground,
+    onBackground: NSColor.darkOnBackground,
+    surface: NSColor.darkSurface,
+    onSurface: NSColor.darkOnSurface,
+    primaryContainer: NSColor.darkPrimaryContainer,
+    onPrimaryContainer: NSColor.darkOnPrimaryContainer,
+    secondaryContainer: NSColor.darkSecondaryContainer,
+    onSecondaryContainer: NSColor.darkOnSecondaryContainer,
+    scrim: NSColor.darkSecondary,
+    tertiary: NSColor.tertiary,
+  );
+}
--- nike_sneaker_store/lib/themes/ns_theme.dart
+++ nike_sneaker_store/lib/themes/ns_theme.dart
@@ -1,52 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:nike_sneaker_store/resources/ns_color.dart';
+import 'package:nike_sneaker_store/themes/ns_color_theme.dart';
+import 'package:nike_sneaker_store/themes/ns_text_theme.dart';
 
 class NSTheme {
   NSTheme._();
 
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
-    colorScheme: const ColorScheme(
-      brightness: Brightness.light,
-      primary: NSColor.primary,
-      onPrimary: NSColor.onPrimary,
-      secondary: NSColor.secondary,
-      onSecondary: NSColor.onSecondary,
-      error: NSColor.notification,
-      onError: NSColor.onPrimary,
-      background: NSColor.background,
-      onBackground: NSColor.onBackground,
-      surface: NSColor.surface,
-      onSurface: NSColor.onSurface,
-      primaryContainer: NSColor.primaryContainer,
-      onPrimaryContainer: NSColor.onPrimaryContainer,
-      secondaryContainer: NSColor.secondaryContainer,
-      onSecondaryContainer: NSColor.onSecondaryContainer,
-      scrim: NSColor.primary,
-      tertiary: NSColor.tertiary,
-    ),
+    colorScheme: NSColorTheme.colorThemeLight,
+    textTheme: NSTextTheme.textTheme,
   );
 
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
-    colorScheme: const ColorScheme(
-      brightness: Brightness.dark,
-      primary: NSColor.darkPrimary,
-      onPrimary: NSColor.darkOnPrimary,
-      secondary: NSColor.darkSecondary,
-      onSecondary: NSColor.darkOnSecondary,
-      error: NSColor.notification,
-      onError: NSColor.onPrimary,
-      background: NSColor.darkBackground,
-      onBackground: NSColor.darkOnBackground,
-      surface: NSColor.darkSurface,
-      onSurface: NSColor.darkOnSurface,
-      primaryContainer: NSColor.darkPrimaryContainer,
-      onPrimaryContainer: NSColor.darkOnPrimaryContainer,
-      secondaryContainer: NSColor.darkSecondaryContainer,
-      onSecondaryContainer: NSColor.darkOnSecondaryContainer,
-      scrim: NSColor.darkSecondary,
-      tertiary: NSColor.tertiary,
-    ),
+    colorScheme: NSColorTheme.colorThemeDark,
+    textTheme: NSTextTheme.textTheme,
+
   );
 }
