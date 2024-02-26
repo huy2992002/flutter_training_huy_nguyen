68cd6885a4c0c49d58157d1708dfcf56d3ecf5ac
huy2992002 <huy2992018@gmail.com>
2024-02-26 16:50:04 +0700

    update gen style & apply in project

--- nike_sneaker_store/lib/resources/ns_style.dart
+++ nike_sneaker_store/lib/resources/ns_style.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nike_sneaker_store/gen/fonts.gen.dart';
 
 class NSStyle {
   static TextStyle primaryTextStyle({
@@ -15,109 +16,109 @@ class NSStyle {
 
   static TextStyle h12Normal = primaryTextStyle(
     fontSize: 12,
-    fontFamily: 'Poppins',
+    fontFamily: FontFamily.poppins,
     fontWeight: FontWeight.w400,
   );
 
   static TextStyle h12Medium = primaryTextStyle(
     fontSize: 12,
-    fontFamily: 'Poppins',
+    fontFamily: FontFamily.poppins,
     fontWeight: FontWeight.w500,
   );
 
   static TextStyle h14Normal = primaryTextStyle(
     fontSize: 14,
-    fontFamily: 'Poppins',
+    fontFamily: FontFamily.poppins,
     fontWeight: FontWeight.w400,
   );
 
   static TextStyle h14Medium = primaryTextStyle(
     fontSize: 14,
-    fontFamily: 'Poppins',
+    fontFamily: FontFamily.poppins,
     fontWeight: FontWeight.w500,
   );
 
   static TextStyle h14SemiBold = primaryTextStyle(
     fontSize: 14,
-    fontFamily: 'Raleway',
+    fontFamily: FontFamily.raleway,
     fontWeight: FontWeight.w600,
   );
 
   static TextStyle h16ExtraLight = primaryTextStyle(
     fontSize: 16,
-    fontFamily: 'Raleway',
+    fontFamily: FontFamily.raleway,
     fontWeight: FontWeight.w200,
   );
 
   static TextStyle h16Normal = primaryTextStyle(
     fontSize: 16,
-    fontFamily: 'Poppins',
+    fontFamily: FontFamily.poppins,
     fontWeight: FontWeight.w400,
   );
 
   static TextStyle h16Medium = primaryTextStyle(
     fontSize: 16,
-    fontFamily: 'Poppins',
+    fontFamily: FontFamily.poppins,
     fontWeight: FontWeight.w500,
   );
 
   static TextStyle h16SemiBold = primaryTextStyle(
     fontSize: 16,
-    fontFamily: 'Raleway',
+    fontFamily: FontFamily.raleway,
     fontWeight: FontWeight.w600,
   );
 
   static TextStyle h16Bold = primaryTextStyle(
     fontSize: 16,
-    fontFamily: 'Raleway',
+    fontFamily: FontFamily.raleway,
     fontWeight: FontWeight.w700,
   );
 
   static TextStyle h18Semibold = primaryTextStyle(
     fontSize: 18,
-    fontFamily: 'Poppins',
+    fontFamily: FontFamily.poppins,
     fontWeight: FontWeight.w600,
   );
 
   static TextStyle h20Medium = primaryTextStyle(
     fontSize: 20,
-    fontFamily: 'Poppins',
+    fontFamily: FontFamily.poppins,
     fontWeight: FontWeight.w500,
   );
 
   static TextStyle h21SemiBold = primaryTextStyle(
     fontSize: 21,
-    fontFamily: 'Raleway',
+    fontFamily: FontFamily.raleway,
     fontWeight: FontWeight.w600,
   );
 
   static TextStyle h24Semibold = primaryTextStyle(
     fontSize: 24,
-    fontFamily: 'Poppins',
+    fontFamily: FontFamily.poppins,
     fontWeight: FontWeight.w600,
   );
 
   static TextStyle h26Bold = primaryTextStyle(
     fontSize: 26,
-    fontFamily: 'Raleway-Bold',
+    fontFamily: FontFamily.ralewayBold,
     fontWeight: FontWeight.w700,
   );
 
   static TextStyle h32Bold = primaryTextStyle(
     fontSize: 32,
-    fontFamily: 'Raleway-Bold',
+    fontFamily: FontFamily.ralewayBold,
     fontWeight: FontWeight.w700,
   );
 
   static TextStyle h32Black = primaryTextStyle(
     fontSize: 32,
-    fontFamily: 'Raleway-Black',
+    fontFamily: FontFamily.ralewayBlack,
     fontWeight: FontWeight.w900,
   );
 
   static TextStyle h36Black = primaryTextStyle(
     fontSize: 36,
-    fontFamily: 'Poppins-Bold',
+    fontFamily: FontFamily.ralewayBold,
     fontWeight: FontWeight.w700,
   );
 }
