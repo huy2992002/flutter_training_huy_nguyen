1124b75d380b508ff46434c7cc404c3850b0f63a
huy2992002 <huy2992018@gmail.com>
2024-02-28 15:47:27 +0700

    remove exmaples pattern

--- nike_sneaker_store/lib/pages/auth/sign_up_page.dart
+++ nike_sneaker_store/lib/pages/auth/sign_up_page.dart
@@ -53,8 +53,7 @@ class _SignUpPageState extends State<SignUpPage> {
   /// If it is already there, the [NSSnackBar] will be displayed.
   /// If it is not in the account, add the user to the account
   Future<void> onRegister() async {
-    if (_formKey.currentState == null || !_formKey.currentState!.validate())
-      return;
+    if (_formKey.currentState == null || !_formKey.currentState!.validate()) return;
     _showHiddenLoading(status: true);
     await Future.delayed(const Duration(seconds: 2));
     bool checkUser = accounts.any((e) => _emailController.text == e.email);
