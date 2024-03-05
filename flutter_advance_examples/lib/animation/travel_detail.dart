34e2e412163bcc07ff848d37761de1563b8f8dc4
huy2992002 <huy2992018@gmail.com>
2024-03-05 15:48:56 +0700

    add examples animation tween

--- flutter_advance_examples/lib/animation/travel_detail.dart
+++ flutter_advance_examples/lib/animation/travel_detail.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_advance_examples/animation/travel_model.dart';
 
-class TravelDetail extends StatelessWidget {
+class TravelDetail extends StatefulWidget {
   const TravelDetail({super.key, required this.travel});
 
   final TravelModel travel;
+
+  @override
+  State<TravelDetail> createState() => _TravelDetailState();
+}
+
+class _TravelDetailState extends State<TravelDetail>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation _colorAnimation;
+  late Animation _sizeAnimation;
+  bool isFavorite = false;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        duration: const Duration(milliseconds: 500), vsync: this);
+    _colorAnimation = ColorTween(begin: Colors.grey, end: Colors.red)
+        .animate(_animationController);
+
+    _sizeAnimation = TweenSequence(
+      [
+        TweenSequenceItem(tween: Tween<double>(begin: 18, end: 22), weight: 50),
+        TweenSequenceItem(tween: Tween<double>(begin: 22, end: 18), weight: 50),
+      ],
+    ).animate(_animationController);
+    _animationController.addListener(() {
+      setState(() {});
+    });
+
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.dismissed) {
+        isFavorite = false;
+      } else {
+        isFavorite = true;
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,9 +53,9 @@ class TravelDetail extends StatelessWidget {
           Stack(
             children: [
               Hero(
-                tag: 'tag ${travel.travelName}',
+                tag: 'tag ${widget.travel.travelName}',
                 child: Image.asset(
-                  travel.image,
+                  widget.travel.image,
                   width: double.infinity,
                   height: 200,
                   fit: BoxFit.cover,
@@ -37,10 +76,29 @@ class TravelDetail extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(travel.travelName),
+                Row(
+                  children: [
+                    Text(widget.travel.travelName),
+                    const Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        if (isFavorite) {
+                          _animationController.reverse();
+                        } else {
+                          _animationController.forward();
+                        }
+                      },
+                      child: Icon(
+                        Icons.favorite_rounded,
+                        color: _colorAnimation.value,
+                        size: _sizeAnimation.value,
+                      ),
+                    ),
+                  ],
+                ),
                 const SizedBox(height: 6),
                 Text(
-                  travel.description,
+                  widget.travel.description,
                   style: const TextStyle(color: Colors.grey),
                 )
               ],
