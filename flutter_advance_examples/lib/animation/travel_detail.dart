ac02409b186657929520d169649e6ec10bc750c8
huy2992002 <huy2992018@gmail.com>
2024-03-07 08:35:27 +0700

    clear package dont use

--- flutter_advance_examples/lib/animation/travel_detail.dart
+++ flutter_advance_examples/lib/animation/travel_detail.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_advance_examples/animation/travel_model.dart';
 
 class TravelDetail extends StatefulWidget {
