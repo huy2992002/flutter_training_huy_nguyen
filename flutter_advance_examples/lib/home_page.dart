23b640d45579c52f352e77cdd02d64481e4a03bc
huy2992002 <huy2992018@gmail.com>
2024-03-25 15:22:45 +0700

    create examples about image picker

--- flutter_advance_examples/lib/home_page.dart
+++ flutter_advance_examples/lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advance_examples/animation/animation_page.dart';
 import 'package:flutter_advance_examples/counter/counter_page/counter_page.dart';
+import 'package:flutter_advance_examples/image_picker/image_picker_page.dart';
 import 'package:flutter_advance_examples/login_form/login_ui/login_page.dart';
 import 'package:flutter_advance_examples/permission_handler/permisson_handler_page.dart';
 import 'package:flutter_advance_examples/posts/pages/posts_page.dart';
@@ -23,6 +24,7 @@ class HomePage extends StatelessWidget {
           TextRoute(text: 'Login Form', page: LoginPage()),
           TextRoute(text: 'Posts', page: PostsPage()),
           TextRoute(text: 'Permission handler', page: PermissionHandlerPage()),
+          TextRoute(text: 'Image Picker', page: ImagePickerPage()),
         ],
       ),
     );
