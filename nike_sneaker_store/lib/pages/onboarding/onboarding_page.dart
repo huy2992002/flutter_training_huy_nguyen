b4aff1d17cec09333fe0f1ebc9bef9d4cd3adb70
huy2992002 <huy2992018@gmail.com>
2024-03-29 10:07:15 +0700

    update provide in onboarding page

--- nike_sneaker_store/lib/pages/onboarding/onboarding_page.dart
+++ nike_sneaker_store/lib/pages/onboarding/onboarding_page.dart
@@ -46,60 +46,63 @@ class OnboardingPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.secondary,
-      body: BlocSelector<OnboardingCubit, int, int>(
-        selector: (state) => state,
-        builder: (context, state) {
-          return Stack(
-            children: [
-              BackgroundOnboarding(
-                child: PageView.builder(
-                  controller: _pageController,
-                  itemCount: _onboardPages.length,
-                  itemBuilder: (_, index) => _onboardPages[index],
-                  onPageChanged: (value) {
-                    context.read<OnboardingCubit>().onChangePage(value);
-                  },
+      body: BlocProvider(
+        create: (context) => OnboardingCubit(),
+        child: BlocSelector<OnboardingCubit, int, int>(
+          selector: (state) => state,
+          builder: (context, state) {
+            return Stack(
+              children: [
+                BackgroundOnboarding(
+                  child: PageView.builder(
+                    controller: _pageController,
+                    itemCount: _onboardPages.length,
+                    itemBuilder: (_, index) => _onboardPages[index],
+                    onPageChanged: (value) {
+                      context.read<OnboardingCubit>().onChangePage(value);
+                    },
+                  ),
                 ),
-              ),
-              Positioned(
-                left: 20,
-                right: 20,
-                bottom: 26,
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: List.generate(
-                        _onboardPages.length,
-                        (index) => AnimatedContainer(
-                          duration: const Duration(milliseconds: 200),
-                          width: state == index ? 42 : 28,
-                          height: 5,
-                          margin: const EdgeInsets.symmetric(horizontal: 3),
-                          decoration: BoxDecoration(
-                            color: state == index
-                                ? Theme.of(context).colorScheme.onSecondary
-                                : Theme.of(context).colorScheme.tertiary,
-                            borderRadius: BorderRadius.circular(5),
+                Positioned(
+                  left: 20,
+                  right: 20,
+                  bottom: 26,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: List.generate(
+                          _onboardPages.length,
+                          (index) => AnimatedContainer(
+                            duration: const Duration(milliseconds: 200),
+                            width: state == index ? 42 : 28,
+                            height: 5,
+                            margin: const EdgeInsets.symmetric(horizontal: 3),
+                            decoration: BoxDecoration(
+                              color: state == index
+                                  ? Theme.of(context).colorScheme.onSecondary
+                                  : Theme.of(context).colorScheme.tertiary,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 36),
-                    NSElevatedButton.text(
-                      onPressed: () => _onNext(state),
-                      backgroundColor: NSColor.background,
-                      textColor: NSColor.onBackground,
-                      text: state == 0
-                          ? AppLocalizations.of(context).getStarted
-                          : AppLocalizations.of(context).next,
-                    ),
-                  ],
+                      const SizedBox(height: 36),
+                      NSElevatedButton.text(
+                        onPressed: () => _onNext(state),
+                        backgroundColor: NSColor.background,
+                        textColor: NSColor.onBackground,
+                        text: state == 0
+                            ? AppLocalizations.of(context).getStarted
+                            : AppLocalizations.of(context).next,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          );
-        },
+              ],
+            );
+          },
+        ),
       ),
     );
   }
