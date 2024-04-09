2b913f81144e0f5938442fab2eb5116912adc0d4
huy2992002 <huy2992018@gmail.com>
2024-04-09 11:47:54 +0700

    add dependency gi_router

--- nike_sneaker_store/lib/features/onboarding/view/onboarding_page.dart
+++ nike_sneaker_store/lib/features/onboarding/view/onboarding_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nike_sneaker_store/components/button/ns_elevated_button.dart';
+import 'package:nike_sneaker_store/features/auth/sign_in/view/sign_in_page.dart';
 import 'package:nike_sneaker_store/features/onboarding/bloc/onboarding_cubit.dart';
 import 'package:nike_sneaker_store/features/onboarding/view/widgets/onboarding_background.dart';
 import 'package:nike_sneaker_store/features/onboarding/view/widgets/onboarding_power.dart';
 import 'package:nike_sneaker_store/features/onboarding/view/widgets/onboarding_start.dart';
 import 'package:nike_sneaker_store/features/onboarding/view/widgets/onboarding_welcome.dart';
 import 'package:nike_sneaker_store/l10n/app_localizations.dart';
-import 'package:nike_sneaker_store/pages/auth/sign_in_page.dart';
 
 import 'package:nike_sneaker_store/resources/ns_color.dart';
 
