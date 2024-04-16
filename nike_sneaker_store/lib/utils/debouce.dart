952d4d73e74976214e4d9799b7ced822e5641b3e
huy2992002 <huy2992018@gmail.com>
2024-04-16 13:56:55 +0700

    rename class Debounce

--- nike_sneaker_store/lib/utils/debouce.dart
+++ nike_sneaker_store/lib/utils/debouce.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
-class Debouncer {
+class Debounce {
+  Debounce({required this.milliseconds});
+  
   final int milliseconds;
   VoidCallback? action;
   Timer? _timer;
-
-  Debouncer({required this.milliseconds});
 
   run(VoidCallback action) {
     if (_timer != null) {
