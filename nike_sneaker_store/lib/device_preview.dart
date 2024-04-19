c75bf9a46aebd33f5cc795d6d7bcc9db2ed627d1
huy2992002 <huy2992018@gmail.com>
2024-04-19 16:45:33 +0700

    update main & device preview

--- nike_sneaker_store/lib/device_preview.dart
+++ nike_sneaker_store/lib/device_preview.dart
@@ -15,8 +15,9 @@ import 'package:nike_sneaker_store/features/notification/bloc/notification_bloc.
 import 'package:nike_sneaker_store/features/notification/bloc/notification_event.dart';
 import 'package:nike_sneaker_store/features/profile/bloc/profile_bloc.dart';
 import 'package:nike_sneaker_store/features/profile/bloc/profile_event.dart';
+import 'package:nike_sneaker_store/features/setting/bloc/setting_bloc.dart';
+import 'package:nike_sneaker_store/features/setting/bloc/setting_state.dart';
 import 'package:nike_sneaker_store/l10n/app_localizations.dart';
-import 'package:nike_sneaker_store/providers/app_provider.dart';
 import 'package:nike_sneaker_store/repository/auth_repository.dart';
 import 'package:nike_sneaker_store/repository/product_repository.dart';
 import 'package:nike_sneaker_store/repository/user_repository.dart';
@@ -27,7 +28,6 @@ import 'package:nike_sneaker_store/services/remote/api_client.dart';
 import 'package:nike_sneaker_store/services/remote/supabase_services.dart';
 import 'package:nike_sneaker_store/themes/ns_theme.dart';
 import 'package:nike_sneaker_store/utils/debounce.dart';
-import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -43,10 +43,7 @@ void main() async {
 
   runApp(
     DevicePreview(
-      builder: (context) => ChangeNotifierProvider(
-        create: (context) => AppProvider(),
-        child: const MyApp(),
-      ),
+      builder: (context) => const MyApp(),
     ),
   );
 }
@@ -57,33 +54,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
-      providers: [
-        RepositoryProvider<SupabaseServices>(
-          create: (context) => SupabaseServices(
-            supabaseClient: Supabase.instance.client,
+        providers: [
+          RepositoryProvider<SupabaseServices>(
+            create: (context) => SupabaseServices(
+              supabaseClient: Supabase.instance.client,
+            ),
           ),
-        ),
-        RepositoryProvider<SharedPrefServices>(
-          create: (context) => SharedPrefServices(
-            sharedPreferences: SharedPreferences.getInstance(),
-          ),
-        ),
-        RepositoryProvider<ApiClient>(
-          create: (context) => ApiClient(
-            dio: Dio(),
-            supabaseClient: Supabase.instance.client,
-            prefs: SharedPrefServices(
+          RepositoryProvider<SharedPrefServices>(
+            create: (context) => SharedPrefServices(
               sharedPreferences: SharedPreferences.getInstance(),
             ),
           ),
-        ),
-        RepositoryProvider<AuthRepository>(
-          create: (context) => AuthRepository(
-            supabaseClient: Supabase.instance.client,
-            sharedPrefServices: SharedPrefServices(
-              sharedPreferences: SharedPreferences.getInstance(),
-            ),
-            apiClient: ApiClient(
+          RepositoryProvider<ApiClient>(
+            create: (context) => ApiClient(
               dio: Dio(),
               supabaseClient: Supabase.instance.client,
               prefs: SharedPrefServices(
@@ -91,80 +74,97 @@ class MyApp extends StatelessWidget {
               ),
             ),
           ),
-        ),
-        RepositoryProvider<ProductRepository>(
-          create: (context) => ProductRepository(
-            apiClient: ApiClient(
-              dio: Dio(),
+          RepositoryProvider<AuthRepository>(
+            create: (context) => AuthRepository(
               supabaseClient: Supabase.instance.client,
-              prefs: context.read<SharedPrefServices>(),
+              sharedPrefServices: SharedPrefServices(
+                sharedPreferences: SharedPreferences.getInstance(),
+              ),
+              apiClient: ApiClient(
+                dio: Dio(),
+                supabaseClient: Supabase.instance.client,
+                prefs: SharedPrefServices(
+                  sharedPreferences: SharedPreferences.getInstance(),
+                ),
+              ),
             ),
           ),
-        ),
-        RepositoryProvider<UserRepository>(
-          create: (context) => UserRepository(
-            apiClient: ApiClient(
-              dio: Dio(),
-              supabaseClient: Supabase.instance.client,
-              prefs: context.read<SharedPrefServices>(),
+          RepositoryProvider<ProductRepository>(
+            create: (context) => ProductRepository(
+              apiClient: ApiClient(
+                dio: Dio(),
+                supabaseClient: Supabase.instance.client,
+                prefs: context.read<SharedPrefServices>(),
+              ),
             ),
           ),
-        ),
-        RepositoryProvider<ZoomDrawerController>(
-          create: (context) => ZoomDrawerController(),
-        ),
-        RepositoryProvider<LayoutCubit>(
-          create: (context) => LayoutCubit(),
-        ),
-        RepositoryProvider<HomeBloc>(
-          create: (context) => HomeBloc(
-            context.read<ProductRepository>(),
-            context.read<AuthRepository>(),
-            context.read<UserRepository>(),
-          )..add(HomeStarted(
-              userId: Supabase.instance.client.auth.currentUser?.id ?? '',
-            )),
-        ),
-        RepositoryProvider<NotificationBloc>(
-          create: (context) => NotificationBloc(
-            context.read<ProductRepository>(),
-            context.read<UserRepository>(),
-          )..add(NotificationStarted(
-              userId: Supabase.instance.client.auth.currentUser?.id ?? '',
-            )),
-        ),
-        RepositoryProvider<ProfileBloc>(
-          create: (context) => ProfileBloc(context.read<UserRepository>())
-            ..add(ProfileStarted(
-              context,
-              name: context.read<HomeBloc>().state.user?.name ?? '',
-              address: context.read<HomeBloc>().state.user?.address ?? '',
-              phoneNumber: context.read<HomeBloc>().state.user?.phone ?? '',
-            )),
-        ),
-        RepositoryProvider<DetailBloc>(create: (context) => DetailBloc()),
-        RepositoryProvider<Debounce>(
-          create: (context) => Debounce(milliseconds: 500),
-        ),
-        RepositoryProvider<CartBloc>(
-          create: (context) => CartBloc(
-            context.read<ProductRepository>(),
-            context.read<UserRepository>(),
-          )..add(CartStarted(
-              userId: Supabase.instance.client.auth.currentUser?.id ?? '',
-            )),
-        ),
-      ],
-      child: MaterialApp.router(
-        title: 'Nike Sneaker Store',
-        debugShowCheckedModeBanner: false,
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        locale: Provider.of<AppProvider>(context).locale,
-        theme: Provider.of<AppProvider>(context).themeData,
-        darkTheme: NSTheme.darkTheme,
-        routerConfig: NSRoutesConfig.goRoute,
-      ),
-    );
+          RepositoryProvider<UserRepository>(
+            create: (context) => UserRepository(
+              apiClient: ApiClient(
+                dio: Dio(),
+                supabaseClient: Supabase.instance.client,
+                prefs: context.read<SharedPrefServices>(),
+              ),
+            ),
+          ),
+          RepositoryProvider<ZoomDrawerController>(
+            create: (context) => ZoomDrawerController(),
+          ),
+          RepositoryProvider<LayoutCubit>(
+            create: (context) => LayoutCubit(),
+          ),
+          RepositoryProvider<HomeBloc>(
+            create: (context) => HomeBloc(
+              context.read<ProductRepository>(),
+              context.read<AuthRepository>(),
+              context.read<UserRepository>(),
+            )..add(HomeStarted(
+                userId: Supabase.instance.client.auth.currentUser?.id ?? '',
+              )),
+          ),
+          RepositoryProvider<NotificationBloc>(
+            create: (context) => NotificationBloc(
+              context.read<ProductRepository>(),
+              context.read<UserRepository>(),
+            )..add(NotificationStarted(
+                userId: Supabase.instance.client.auth.currentUser?.id ?? '',
+              )),
+          ),
+          RepositoryProvider<ProfileBloc>(
+            create: (context) => ProfileBloc(context.read<UserRepository>())
+              ..add(ProfileStarted(
+                context,
+                name: context.read<HomeBloc>().state.user?.name ?? '',
+                address: context.read<HomeBloc>().state.user?.address ?? '',
+                phoneNumber: context.read<HomeBloc>().state.user?.phone ?? '',
+              )),
+          ),
+          RepositoryProvider<DetailBloc>(create: (context) => DetailBloc()),
+          RepositoryProvider<Debounce>(
+            create: (context) => Debounce(milliseconds: 500),
+          ),
+          RepositoryProvider<CartBloc>(
+            create: (context) => CartBloc(
+              context.read<ProductRepository>(),
+              context.read<UserRepository>(),
+            )..add(CartStarted(
+                userId: Supabase.instance.client.auth.currentUser?.id ?? '',
+              )),
+          ),
+        ],
+        child: BlocBuilder<SettingBloc, SettingState>(
+          builder: (context, state) {
+            return MaterialApp.router(
+              title: 'Nike Sneaker Store',
+              debugShowCheckedModeBanner: false,
+              localizationsDelegates: AppLocalizations.localizationsDelegates,
+              supportedLocales: AppLocalizations.supportedLocales,
+              locale: state.locale,
+              theme: state.themeData,
+              darkTheme: NSTheme.darkTheme,
+              routerConfig: NSRoutesConfig.goRoute,
+            );
+          },
+        ));
   }
 }
