e65f92c352cc58306c25df057565a8650f85ed2f
huy2992002 <huy2992018@gmail.com>
2024-04-22 13:55:57 +0700

    update shared preference

--- nike_sneaker_store/lib/services/local/shared_pref.dart
+++ nike_sneaker_store/lib/services/local/shared_pref.dart
@@ -1,10 +1,6 @@
-import 'dart:convert';
-import 'package:nike_sneaker_store/models/user_model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPrefs {
-  static const String usersKey = 'usersKey';
-  static const String userLoginKey = 'userLoginKey';
   static const String checkAccessKey = 'checkAccessKey';
   static const String themeKey = 'themeKey';
   static const String languageKey = 'languageKey';
@@ -13,37 +9,6 @@ class SharedPrefs {
 
   static Future<void> initialization() async {
     _prefs = await SharedPreferences.getInstance();
-  }
-
-  static Future<List<UserModel>?> getUsers() async {
-    String? data = _prefs.getString(usersKey);
-    if (data == null) return null;
-    final maps = jsonDecode(data).cast<Map<String, dynamic>>()
-        as List<Map<String, dynamic>>;
-    return maps.map(UserModel.fromJson).toList();
-  }
-
-  static Future<void> saveUsers(List<UserModel> users) async {
-    final maps = users.map((e) => e.toJson()).toList();
-    _prefs.setString(usersKey, jsonEncode(maps));
-  }
-
-  static UserModel? get userLogin {
-    String? data = _prefs.getString(userLoginKey);
-    if (data == null) return null;
-    UserModel user =
-        UserModel.fromJson(jsonDecode(data) as Map<String, dynamic>);
-    return user;
-  }
-
-  static set userLogin(UserModel? user) {
-    if (user != null) {
-      _prefs.setString(userLoginKey, jsonEncode(user.toJson()));
-    }
-  }
-
-  static void removeUserLogin() {
-    _prefs.remove(userLoginKey);
   }
 
   static bool get isAccessed {
