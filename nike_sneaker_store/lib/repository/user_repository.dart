f7cc3699188968a59f248358768234be0d9ba45f
huy2992002 <huy2992018@gmail.com>
2024-05-06 11:07:06 +0700

    update UserRepository use SupabaseServices

--- nike_sneaker_store/lib/repository/user_repository.dart
+++ nike_sneaker_store/lib/repository/user_repository.dart
@@ -2,18 +2,25 @@ import 'dart:io';
 import 'package:nike_sneaker_store/constants/ns_constants.dart';
 import 'package:nike_sneaker_store/models/user_model.dart';
 import 'package:nike_sneaker_store/services/remote/api_client.dart';
+import 'package:nike_sneaker_store/services/remote/supabase_services.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class UserRepository {
   UserRepository({
+    required this.supabaseServices,
     required this.apiClient,
   });
+
+  final SupabaseServices supabaseServices;
   final ApiClient apiClient;
 
   Future<void> updateInformationUser(UserModel user) async {
     try {
-      final url = '${NSConstants.endPointUsers}?uuid=eq.${user.uuid}';
-      apiClient.patch(url, data: user.toJson());
+      // final url = '${NSConstants.endPointUsers}?uuid=eq.${user.uuid}';
+      // apiClient.patch(url, data: user.toJson());
+      final query = await supabaseServices.updateData(
+          table: NSConstants.tableUsers, values: user.toJson());
+      await query.eq('uuid', user.uuid ?? '');
     } catch (e) {
       rethrow;
     }
@@ -31,11 +38,11 @@ class UserRepository {
     required String userId,
   }) async {
     try {
-      final url = '${NSConstants.endPointUsers}?uuid=eq.$userId';
-      final response = await apiClient.get(url);
-      final data = response.data as List<dynamic>;
-      if (data.isNotEmpty && data[0] is Map<String, dynamic>) {
-        return UserModel.fromJson(data[0] as Map<String, dynamic>);
+      final query =
+          await supabaseServices.fetchData(table: NSConstants.tableUsers);
+      final data = await query.eq('uuid', userId).select();
+      if (data.isNotEmpty) {
+        return UserModel.fromJson(data.first);
       } else {
         throw const AuthException('User not found');
       }
