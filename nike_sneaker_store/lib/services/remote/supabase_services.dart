cbfb349124b58c49fabfc0892c98afb502cc3f51
huy2992002 <huy2992018@gmail.com>
2024-05-06 10:42:56 +0700

    create supabase serrvices

--- nike_sneaker_store/lib/services/remote/supabase_services.dart
+++ nike_sneaker_store/lib/services/remote/supabase_services.dart
@@ -5,4 +5,38 @@ class SupabaseServices {
 
   final SupabaseClient supabaseClient;
 
+  Future<PostgrestFilterBuilder<dynamic>> _safeSupabaseRequest(
+      PostgrestFilterBuilder<dynamic> Function() request) async {
+    try {
+      return request();
+    } catch (e) {
+      if (e is PostgrestException && e.code == '401') {
+        // Refresh the session
+        await supabaseClient.auth
+            .refreshSession()
+            .onError((error, stackTrace) => throw e);
+        // Retry the request
+        return request();
+      } else {
+        // It's not a 401 error, rethrow it
+        rethrow;
+      }
+    }
+  }
+
+  Future<PostgrestFilterBuilder<dynamic>> fetchData({
+    required String table,
+    String? select,
+  }) {
+    return _safeSupabaseRequest(
+        () => supabaseClient.from(table).select(select ?? '*'));
+  }
+
+  Future<PostgrestFilterBuilder<dynamic>> updateData({
+    required String table,
+    required Map<String, dynamic> values,
+  }) {
+    return _safeSupabaseRequest(
+        () => supabaseClient.from(table).update(values));
+  }
 }
