7b788ebd84aab2834d6f6a66dacc739d4bfc2914
huy2992002 <huy2992018@gmail.com>
2024-05-07 00:34:42 +0700

    create responsive in comenent appbar

--- nike_sneaker_store/lib/components/app_bar/action_icon_app_bar.dart
+++ nike_sneaker_store/lib/components/app_bar/action_icon_app_bar.dart
@@ -4,6 +4,7 @@ import 'package:go_router/go_router.dart';
 import 'package:nike_sneaker_store/components/button/ns_icon_button.dart';
 import 'package:nike_sneaker_store/gen/assets.gen.dart';
 import 'package:nike_sneaker_store/routes/ns_routes_const.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class ActionIconAppBar extends StatelessWidget {
   /// Cart icon in AppBar with action to go to your cart
@@ -23,6 +24,11 @@ class ActionIconAppBar extends StatelessWidget {
           onPressed: () => context.push(NSRoutesConst.pathCart),
           icon: SvgPicture.asset(
             Assets.icons.icBag,
+            width: getValueForScreenType(
+              context: context,
+              mobile: 24,
+              tablet: 28,
+            ),
             color: Theme.of(context).colorScheme.onBackground,
           ),
           backgroundColor: Theme.of(context).colorScheme.primaryContainer,
--- nike_sneaker_store/lib/components/app_bar/app_bar_home.dart
+++ nike_sneaker_store/lib/components/app_bar/app_bar_home.dart
@@ -1,34 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import 'package:nike_sneaker_store/components/app_bar/action_icon_app_bar.dart';
 import 'package:nike_sneaker_store/gen/assets.gen.dart';
 import 'package:nike_sneaker_store/l10n/app_localizations.dart';
-import 'package:provider/provider.dart';
 
 class AppBarHome extends StatelessWidget implements PreferredSizeWidget {
   /// Create design app bar with height 50px
   const AppBarHome({
     this.isMarkerNotification = false,
+    this.onMenu,
     super.key,
   });
 
   /// The [isMarkerNotification] argument is true will display marker with notification
   final bool isMarkerNotification;
 
+  final Function()? onMenu;
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      leading: GestureDetector(
-        onTap: () => context.read<ZoomDrawerController>().open?.call(),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 20),
-          child: SvgPicture.asset(
-            Assets.icons.icMenu,
-            color: Theme.of(context).colorScheme.onBackground,
-          ),
-        ),
-      ),
+      leading: onMenu != null
+          ? GestureDetector(
+              onTap: onMenu,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: SvgPicture.asset(
+                  Assets.icons.icMenu,
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+              ),
+            )
+          : null,
       leadingWidth: 43,
       title: Text(
         AppLocalizations.of(context).explore,
