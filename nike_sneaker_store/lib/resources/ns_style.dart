f5fb36a04d73ed0b107b94cd7f945c9aa44d826e
huy2992002 <huy2992018@gmail.com>
2024-05-07 00:15:04 +0700

    update TextTheme Responsive

--- nike_sneaker_store/lib/resources/ns_style.dart
+++ nike_sneaker_store/lib/resources/ns_style.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nike_sneaker_store/gen/fonts.gen.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class NSStyle {
   static TextStyle primaryTextStyle({
@@ -14,111 +15,183 @@ class NSStyle {
     );
   }
 
-  static TextStyle h12Normal = primaryTextStyle(
-    fontSize: 12,
-    fontFamily: FontFamily.poppins,
-    fontWeight: FontWeight.w400,
-  );
+  TextStyle h12Normal(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 12,
+          tablet: 16,
+        ),
+        fontFamily: FontFamily.poppins,
+        fontWeight: FontWeight.w400,
+      );
 
-  static TextStyle h12Medium = primaryTextStyle(
-    fontSize: 12,
-    fontFamily: FontFamily.poppins,
-    fontWeight: FontWeight.w500,
-  );
+  TextStyle h12Medium(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 12,
+          tablet: 16,
+        ),
+        fontFamily: FontFamily.poppins,
+        fontWeight: FontWeight.w500,
+      );
 
-  static TextStyle h14Normal = primaryTextStyle(
-    fontSize: 14,
-    fontFamily: FontFamily.poppins,
-    fontWeight: FontWeight.w400,
-  );
+  TextStyle h14Normal(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 14,
+          tablet: 18,
+        ),
+        fontFamily: FontFamily.poppins,
+        fontWeight: FontWeight.w400,
+      );
 
-  static TextStyle h14Medium = primaryTextStyle(
-    fontSize: 14,
-    fontFamily: FontFamily.poppins,
-    fontWeight: FontWeight.w500,
-  );
+  TextStyle h14Medium(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 14,
+          tablet: 18,
+        ),
+        fontFamily: FontFamily.poppins,
+        fontWeight: FontWeight.w500,
+      );
 
-  static TextStyle h14SemiBold = primaryTextStyle(
-    fontSize: 14,
-    fontFamily: FontFamily.raleway,
-    fontWeight: FontWeight.w600,
-  );
+  TextStyle h14SemiBold(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 14,
+          tablet: 18,
+        ),
+        fontFamily: FontFamily.raleway,
+        fontWeight: FontWeight.w600,
+      );
 
-  static TextStyle h16ExtraLight = primaryTextStyle(
-    fontSize: 16,
-    fontFamily: FontFamily.raleway,
-    fontWeight: FontWeight.w200,
-  );
+  TextStyle h16ExtraLight(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 16,
+          tablet: 20,
+        ),
+        fontFamily: FontFamily.raleway,
+        fontWeight: FontWeight.w200,
+      );
 
-  static TextStyle h16Normal = primaryTextStyle(
-    fontSize: 16,
-    fontFamily: FontFamily.poppins,
-    fontWeight: FontWeight.w400,
-  );
+  TextStyle h16Normal(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 16,
+          tablet: 20,
+        ),
+        fontFamily: FontFamily.poppins,
+        fontWeight: FontWeight.w400,
+      );
 
-  static TextStyle h16Medium = primaryTextStyle(
-    fontSize: 16,
-    fontFamily: FontFamily.poppins,
-    fontWeight: FontWeight.w500,
-  );
+  TextStyle h16Medium(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 16,
+          tablet: 20,
+        ),
+        fontFamily: FontFamily.poppins,
+        fontWeight: FontWeight.w500,
+      );
 
-  static TextStyle h16SemiBold = primaryTextStyle(
-    fontSize: 16,
-    fontFamily: FontFamily.raleway,
-    fontWeight: FontWeight.w600,
-  );
+  TextStyle h16SemiBold(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 16,
+          tablet: 20,
+        ),
+        fontFamily: FontFamily.raleway,
+        fontWeight: FontWeight.w600,
+      );
 
-  static TextStyle h16Bold = primaryTextStyle(
-    fontSize: 16,
-    fontFamily: FontFamily.raleway,
-    fontWeight: FontWeight.w700,
-  );
+  TextStyle h16Bold(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 16,
+          tablet: 20,
+        ),
+        fontFamily: FontFamily.raleway,
+        fontWeight: FontWeight.w700,
+      );
 
-  static TextStyle h18Semibold = primaryTextStyle(
-    fontSize: 18,
-    fontFamily: FontFamily.poppins,
-    fontWeight: FontWeight.w600,
-  );
+  TextStyle h18Semibold(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 18,
+          tablet: 22,
+        ),
+        fontFamily: FontFamily.poppins,
+        fontWeight: FontWeight.w600,
+      );
 
-  static TextStyle h20Medium = primaryTextStyle(
-    fontSize: 20,
-    fontFamily: FontFamily.poppins,
-    fontWeight: FontWeight.w500,
-  );
+  TextStyle h20Medium(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 20,
+          tablet: 24,
+        ),
+        fontFamily: FontFamily.poppins,
+        fontWeight: FontWeight.w500,
+      );
 
-  static TextStyle h21SemiBold = primaryTextStyle(
-    fontSize: 21,
-    fontFamily: FontFamily.raleway,
-    fontWeight: FontWeight.w600,
-  );
+  TextStyle h21SemiBold(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 21,
+          tablet: 25,
+        ),
+        fontFamily: FontFamily.raleway,
+        fontWeight: FontWeight.w600,
+      );
 
-  static TextStyle h24Semibold = primaryTextStyle(
-    fontSize: 24,
-    fontFamily: FontFamily.poppins,
-    fontWeight: FontWeight.w600,
-  );
+  TextStyle h24Semibold(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 24,
+          tablet: 28,
+        ),
+        fontFamily: FontFamily.poppins,
+        fontWeight: FontWeight.w600,
+      );
 
-  static TextStyle h26Bold = primaryTextStyle(
-    fontSize: 26,
-    fontFamily: FontFamily.ralewayBold,
-    fontWeight: FontWeight.w700,
-  );
+  TextStyle h26Bold(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 26,
+          tablet: 30,
+        ),
+        fontFamily: FontFamily.ralewayBold,
+        fontWeight: FontWeight.w700,
+      );
 
-  static TextStyle h32Bold = primaryTextStyle(
-    fontSize: 32,
-    fontFamily: FontFamily.ralewayBold,
-    fontWeight: FontWeight.w700,
-  );
+  TextStyle h32Bold(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 32,
+          tablet: 36,
+        ),
+        fontFamily: FontFamily.ralewayBold,
+        fontWeight: FontWeight.w700,
+      );
 
-  static TextStyle h32Black = primaryTextStyle(
-    fontSize: 32,
-    fontFamily: FontFamily.ralewayBlack,
-    fontWeight: FontWeight.w900,
-  );
+  TextStyle h32Black(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 32,
+          tablet: 36,
+        ),
+        fontFamily: FontFamily.ralewayBlack,
+        fontWeight: FontWeight.w900,
+      );
 
-  static TextStyle h33Black = primaryTextStyle(
-    fontSize: 33,
-    fontFamily: FontFamily.poppinsBold,
-    fontWeight: FontWeight.w700,
-  );
+  TextStyle h33Black(BuildContext context) => primaryTextStyle(
+        fontSize: getValueForScreenType(
+          context: context,
+          mobile: 33,
+          tablet: 37,
+        ),
+        fontFamily: FontFamily.poppinsBold,
+        fontWeight: FontWeight.w700,
+      );
 }
--- nike_sneaker_store/lib/themes/ns_theme.dart
+++ nike_sneaker_store/lib/themes/ns_theme.dart
@@ -5,39 +5,50 @@ import 'package:nike_sneaker_store/themes/ns_color_theme.dart';
 import 'package:nike_sneaker_store/themes/ns_text_theme.dart';
 
 class NSTheme {
-  NSTheme._();
+  NSTheme();
 
-  static ThemeData lightTheme = ThemeData(
-    useMaterial3: true,
-    brightness: Brightness.light,
-    colorScheme: NSColorTheme.colorThemeLight,
-    textTheme: NSTextTheme.textTheme,
-    appBarTheme: AppBarTheme(
-      backgroundColor: NSColor.background,
-      surfaceTintColor: Colors.transparent,
-      centerTitle: true,
-      titleTextStyle: NSStyle.h16SemiBold.copyWith(color: NSColor.onBackground),
-    ),
-    backgroundColor: NSColor.background,
-    shadowColor: NSColor.onPrimaryContainer.withOpacity(0.4),
-    dialogBackgroundColor: NSColor.background,
-  );
+  ThemeData lightTheme(BuildContext context) {
+    NSTextTheme nsTextTheme = NSTextTheme();
+    NSStyle nsStyle = NSStyle();
 
-  static ThemeData darkTheme = ThemeData(
-    useMaterial3: true,
-    brightness: Brightness.dark,
-    colorScheme: NSColorTheme.colorThemeDark,
-    textTheme: NSTextTheme.textTheme,
-    appBarTheme: AppBarTheme(
-      backgroundColor: NSColor.darkBackground,
-      surfaceTintColor: Colors.transparent,
-      centerTitle: true,
-      titleTextStyle: NSStyle.h16SemiBold.copyWith(
-        color: NSColor.darkOnBackground,
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.light,
+      colorScheme: NSColorTheme.colorThemeLight,
+      textTheme: nsTextTheme.textTheme(context),
+      appBarTheme: AppBarTheme(
+        backgroundColor: NSColor.background,
+        surfaceTintColor: Colors.transparent,
+        centerTitle: true,
+        titleTextStyle:
+            nsStyle.h16SemiBold(context).copyWith(color: NSColor.onBackground),
       ),
-    ),
-    backgroundColor: NSColor.darkBackground,
-    shadowColor: NSColor.onPrimaryContainer.withOpacity(0.4),
-    dialogBackgroundColor: NSColor.darkBackground,
-  );
+      backgroundColor: NSColor.background,
+      shadowColor: NSColor.onPrimaryContainer.withOpacity(0.4),
+      dialogBackgroundColor: NSColor.background,
+    );
+  }
+
+  ThemeData darkTheme(BuildContext context) {
+    NSTextTheme nsTextTheme = NSTextTheme();
+    NSStyle nsStyle = NSStyle();
+
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      colorScheme: NSColorTheme.colorThemeDark,
+      textTheme: nsTextTheme.textTheme(context),
+      appBarTheme: AppBarTheme(
+        backgroundColor: NSColor.darkBackground,
+        surfaceTintColor: Colors.transparent,
+        centerTitle: true,
+        titleTextStyle: nsStyle.h16SemiBold(context).copyWith(
+              color: NSColor.darkOnBackground,
+            ),
+      ),
+      backgroundColor: NSColor.darkBackground,
+      shadowColor: NSColor.onPrimaryContainer.withOpacity(0.4),
+      dialogBackgroundColor: NSColor.darkBackground,
+    );
+  }
 }
