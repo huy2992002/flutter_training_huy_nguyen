59e45f81753bd7063ce1351015ae9cbfd0f814c7
huy2992002 <huy2992018@gmail.com>
2024-05-07 15:08:18 +0700

    update HomeBlocTest

--- nike_sneaker_store/test/features/home/home_bloc_test.dart
+++ nike_sneaker_store/test/features/home/home_bloc_test.dart
@@ -6,19 +6,16 @@ import 'package:nike_sneaker_store/features/home/bloc/home_state.dart';
 import 'package:nike_sneaker_store/repository/product_repository.dart';
 import 'package:nike_sneaker_store/repository/user_repository.dart';
 import 'package:nike_sneaker_store/utils/enum.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../repository/mock_product_repository.dart';
 import '../../repository/mock_user_repository.dart';
 import '../../utils/mock_data.dart';
-import '../../utils/mock_supabase.dart';
 
 void main() {
   late ProductRepository productRepository;
   late UserRepository userRepository;
 
   late HomeBloc homeBloc;
-  late SupabaseClient supabaseClient;
 
   setUp(() {
     productRepository = MockProductRepository();
@@ -27,7 +24,6 @@ void main() {
       productRepository,
       userRepository,
     );
-    supabaseClient = MockSupabase();
   });
 
   group('Home Bloc Test', () {
@@ -40,7 +36,7 @@ void main() {
       build: () => homeBloc,
       act: (bloc) {
         bloc.add(
-            HomeStarted(userId: supabaseClient.auth.currentUser?.id ?? ''));
+            HomeStarted(userId: 'userId'));
       },
       expect: () => [
         const HomeState(homeStatus: HomeViewStatus.loading),
@@ -49,6 +45,22 @@ void main() {
           productDisplays: MockData.mockProducts,
           user: MockData.mockUser,
           homeStatus: HomeViewStatus.success,
+        ),
+      ],
+    );
+
+    blocTest(
+      'emit status loading & emit message error when fetch failure',
+      build: () => homeBloc,
+      act: (bloc) {
+        bloc.add(
+            HomeStarted(userId: ''));
+      },
+      expect: () => [
+        const HomeState(homeStatus: HomeViewStatus.loading),
+        const HomeState(
+          homeStatus: HomeViewStatus.failure,
+          errorMessage: 'Exception: An error occurred, please check UserId'
         ),
       ],
     );
