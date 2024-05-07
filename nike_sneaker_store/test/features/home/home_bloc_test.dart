26fe96efeb3ac6a3f75bb34d142c85df874758bb
huy2992002 <huy2992018@gmail.com>
2024-05-08 02:17:27 +0700

    update test case in HomeBlocTest

--- nike_sneaker_store/test/features/home/home_bloc_test.dart
+++ nike_sneaker_store/test/features/home/home_bloc_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:nike_sneaker_store/features/home/bloc/home_bloc.dart';
 import 'package:nike_sneaker_store/features/home/bloc/home_event.dart';
 import 'package:nike_sneaker_store/features/home/bloc/home_state.dart';
+import 'package:nike_sneaker_store/models/user_model.dart';
 import 'package:nike_sneaker_store/repository/product_repository.dart';
 import 'package:nike_sneaker_store/repository/user_repository.dart';
 import 'package:nike_sneaker_store/utils/enum.dart';
@@ -35,8 +36,7 @@ void main() {
       'emit status loading & emit data when fetch success',
       build: () => homeBloc,
       act: (bloc) {
-        bloc.add(
-            HomeStarted(userId: 'userId'));
+        bloc.add(HomeStarted(userId: 'userId'));
       },
       expect: () => [
         const HomeState(homeStatus: HomeViewStatus.loading),
@@ -50,18 +50,54 @@ void main() {
     );
 
     blocTest(
+      'emits LoadMoreStatus when load more product success',
+      build: () => homeBloc,
+      act: (bloc) {
+        bloc.add(HomeLoadMore(userId: 'userId', types: const ['types']));
+      },
+      expect: () => [
+        const HomeState(
+          loadStatus: HomeLoadMoreStatus.loading,
+        ),
+        HomeState(
+          loadStatus: HomeLoadMoreStatus.loadSuccess,
+          products: MockData.mockProducts,
+          maxItem: 6,
+        ),
+      ],
+    );
+
+    blocTest(
+      'emits LoadMoreStatus failure when load more product fail',
+      build: () => homeBloc,
+      seed: () => HomeState(products: MockData.mockProducts),
+      act: (bloc) {
+        bloc.add(HomeFavoritePressed(userId: '', productId: '12345'));
+      },
+      expect: () => [
+        HomeState(
+          favoriteStatus: HomeFavoriteStatus.favoriteLoading,
+          products: MockData.mockProducts,
+        ),
+        HomeState(
+          favoriteStatus: HomeFavoriteStatus.favoriteFailure,
+          products: MockData.mockProducts,
+          errorMessage: 'Exception: An error occurred, please check UserId',
+        ),
+      ],
+    );
+
+    blocTest(
       'emit status loading & emit message error when fetch failure',
       build: () => homeBloc,
       act: (bloc) {
-        bloc.add(
-            HomeStarted(userId: ''));
+        bloc.add(HomeStarted(userId: ''));
       },
       expect: () => [
         const HomeState(homeStatus: HomeViewStatus.loading),
         const HomeState(
-          homeStatus: HomeViewStatus.failure,
-          errorMessage: 'Exception: An error occurred, please check UserId'
-        ),
+            homeStatus: HomeViewStatus.failure,
+            errorMessage: 'Exception: An error occurred, please check UserId'),
       ],
     );
 
@@ -84,6 +120,69 @@ void main() {
           products: MockData.mockProducts,
           productDisplays: MockData.mockProducts,
         ),
+      ],
+    );
+
+    blocTest(
+      'emits FavoriteStatus success when favorite product',
+      build: () => homeBloc,
+      seed: () => HomeState(products: MockData.mockProducts),
+      act: (bloc) {
+        bloc.add(HomeFavoritePressed(userId: 'userId', productId: '12345'));
+      },
+      expect: () => [
+        HomeState(
+          favoriteStatus: HomeFavoriteStatus.favoriteLoading,
+          products: MockData.mockProducts,
+        ),
+        HomeState(
+          favoriteStatus: HomeFavoriteStatus.favoriteSuccess,
+          productDisplays: MockData.mockProducts,
+          products: MockData.mockProducts,
+        ),
+      ],
+    );
+
+    blocTest(
+      'emits FavoriteStatus failure when favorite product fail',
+      build: () => homeBloc,
+      seed: () => HomeState(products: MockData.mockProducts),
+      act: (bloc) {
+        bloc.add(HomeFavoritePressed(userId: '', productId: '12345'));
+      },
+      expect: () => [
+        HomeState(
+          favoriteStatus: HomeFavoriteStatus.favoriteLoading,
+          products: MockData.mockProducts,
+        ),
+        HomeState(
+          favoriteStatus: HomeFavoriteStatus.favoriteFailure,
+          products: MockData.mockProducts,
+          errorMessage: 'Exception: An error occurred, please check UserId',
+        ),
+      ],
+    );
+
+    blocTest(
+      'emits value [user] when profile changed',
+      build: () => homeBloc,
+      seed: () => HomeState(user: UserModel()),
+      act: (bloc) {
+        bloc.add(
+          HomeUserChanged(
+            name: 'name',
+            address: 'address',
+            avatar: 'avatar',
+          ),
+        );
+      },
+      expect: () => [
+        HomeState(
+            user: UserModel(
+          name: 'name',
+          address: 'address',
+          avatar: 'avatar',
+        )),
       ],
     );
   });
